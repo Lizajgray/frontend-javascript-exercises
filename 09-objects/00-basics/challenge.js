f4d69f61088a101cf22b0ee5a2fcb37d3f858ab6
Liza Gray <lizajgray6590@gmail.com>
2014-11-20 05:13:08 +0000

    finished objects

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,40 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function (arg1, arg2, arg3) {
+var courseProp = { 
+	title: arg1,
+	duration: arg2,
+	students: arg3
+}
+ return courseProp ;
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function (object, newProp, newValue) {
+var newObject = object ;
+if (object.hasOwnProperty(newProp)) {
+		return newObject;
+	}
+else {
+	newObject[newProp] = newValue 
+	return newObject;
+}
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function (letter) {
+var newLetter = {
+recipient: letter.recipient,
+sender: letter.sender,
+msg: letter.msg
+};
+return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender ;
+};
 
-module.exports.canIGet = undefined;
+
+
+module.exports.canIGet = function (item, money) {
+var prices = {
+	'MacBook Air': 999,
+	'MacBook Pro': 1299,
+	'Mac Pro': 2499,
+	'Apple Sticker': 1
+};
+return prices[item] <= money ;
+};
